be99f2d6b04453744b35265673c391eaf0904226
PZon <piotrzon@gmail.com>
2019-02-12 12:33:04 +0000

    Added f. register user to File

--- KsAdresowa.h
+++ KsAdresowa.h
@@ -7,11 +7,20 @@ class KsiazkaAdresowa{
     int idZalogowanegoUzytkownika;
     int idOstatniegoAdresata;
     int idUsunietegoAdresata;
+    string nazwaPlikuZUzytkownikami;
+
 
     vector<Uzytkownik>uzytkownicy;
     Uzytkownik podajDaneNowegoUzytkownika();
     int pobierzIdNowegoUzytkownika();
+    void dopiszUzytkownikaDoPliku(Uzytkownik uzytkownik);
+    string zamienDaneUzytkownikaNaLinieZDanymiOddzielonaPionowymiKreskami(Uzytkownik uzytkownik);
+    string konwerjsaIntNaString(int liczba);
+    bool czyPlikJestPusty(fstream &plikTekstowy);
+
+
 public:
+    KsiazkaAdresowa();
     void rejestracjaUzytkownika();
     bool czyIstniejeLogin(string login);
     void wypiszWszystkichUzytkownikow();
